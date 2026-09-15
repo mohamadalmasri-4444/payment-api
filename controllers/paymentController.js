b8854f842ca1f9bab5440f18db67b3bd751bdb54
Mohamad <mohamadalmasri4444@gmail.com>
2026-09-15 03:08:19 +0300

    fix: mongoose import in Payment model

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -15,7 +15,8 @@ exports.handlePayment = async (req, res) => {
     method,
     subscriberId,
     payment_status: false, // بانتظار التأكيد
-    time: new Date().toISOString()
+    time: new Date().toLocaleString("ar-SY", { timeZone: "Asia/Damascus" })
+
   };
 
   try {
